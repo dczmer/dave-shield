0b529e5c82451f321b750cc0a909008520d99d5e
Dave Czmer <dczmer@gmail.com>
2026-04-11 13:51:09 -0400

    Restructure jail.nix implementation as a wrapper library instead of bundling with the agent.

--- flake.nix
+++ flake.nix
@@ -2,14 +2,12 @@
   description = "Flake template";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    llm-agents.url = "github:numtide/llm-agents.nix";
     jail-nix.url = "sourcehut:~alexdavid/jail.nix";
   };
   outputs =
     {
       nixpkgs,
       flake-utils,
-      llm-agents,
       jail-nix,
       ...
     }:
@@ -20,32 +18,41 @@
           inherit system;
           config.allowUnfree = true;
         };
-        jailed-agents = import ./packages/agent-jail {
-          inherit
-            pkgs
-            system
-            llm-agents
-            jail-nix
-            ;
+        jail = jail-nix.lib.init pkgs;
+        jailMeLib = import ./packages/jail-me.nix {
+          inherit pkgs jail;
+        };
+        daveShield = jailMeLib.init {
+          name = "dave-shield";
         };
       in
       {
-        lib = with jailed-agents.lib; {
-          makeJailedOpenCode = makeJailedOpenCode;
-          makeJailedShell = makeJailedShell;
+        lib = {
+          # use daveShield for shared persistent home dir across applications.
+          daveShield = daveShield;
+          # use jailMe.init "name" to create an env with a different name and home dir.
+          jailMeLib = jailMeLib;
+          # use the same combinators from the version this flake is using:
+          jailCombinators = jail.combinators;
         };
-        apps = {
-          opencode-daily = {
-            type = "app";
-            program = "${llm-agents.packages.${system}.opencode}/bin/opencode";
-          };
-          jailed-opencode-daily = {
-            type = "app";
-            program = "${jailed-agents.packages.jailed-opencode}/bin/jailed-opencode";
-          };
-          jailed-shell = {
-            type = "app";
-            program = "${jailed-agents.packages.jailed-shell}/bin/jailed-shell";
+        packages = {
+          # example use of daveShield interface:
+          jailedShell = daveShield {
+            # executable to sand-box
+            exec = pkgs.bash;
+            # extra packages to make available
+            extraPkgs = with pkgs; [
+              nethack
+              iputils
+            ];
+            # additional combinators to customize
+            extraCombinators = with jail.combinators; [
+              (wrap-entry (entry: ''
+                echo 'Inside the jail!'
+                ${entry}
+                echo 'Cleaning up...'
+              ''))
+            ];
           };
         };
       }
---
+++ packages/jail-me.nix
@@ -0,0 +1,65 @@
+{
+  pkgs,
+  jail,
+  ...
+}:
+{
+  init =
+    {
+      name ? "dave-shield",
+      hostNetwork ? true,
+    }:
+    let
+      commonPkgs = with pkgs; [
+        bashInteractive
+        curl
+        wget
+        jq
+        git
+        which
+        ripgrep
+        gnugrep
+        gawkInteractive
+        ps
+        findutils
+        gzip
+        unzip
+        gnutar
+        diffutils
+        coreutils
+      ];
+      networkCombinator =
+        if hostNetwork then
+          with jail.combinators;
+          [
+            network
+            (set-hostname name)
+          ]
+        else
+          [ ];
+      jailCombinators =
+        extraPkgs:
+        with jail.combinators;
+        (
+          [
+            time-zone
+            no-new-session
+            # keep a persistent home dir at:
+            # `~/.local/share/jail.nix/${name}`
+            (persist-home name)
+            # mount-cwd AFTER persist-home or it will conflict
+            mount-cwd
+            # add these pkgs bin/ directories to $path
+            (add-pkg-deps commonPkgs)
+            (add-pkg-deps extraPkgs)
+          ]
+          ++ networkCombinator
+        );
+    in
+    {
+      exec,
+      extraPkgs ? [ ],
+      extraCombinators ? [ ],
+    }:
+    jail name exec ((jailCombinators extraPkgs) ++ extraCombinators);
+}
